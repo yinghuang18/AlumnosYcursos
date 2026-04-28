357271b8b4515db2e2254ccb22f928ce633e90f6
POO <y.huang01@ufromail.cl>
2026-04-28 15:41:51 -0400

    constructores con y sin parámetros.

--- src/Alumno.java
+++ src/Alumno.java
@@ -3,9 +3,27 @@ public class Alumnos {
     private String apellido;
     private String matricula;
     private String carrera;
+        public Alumno(String nombre, String apellido, String matricula, String carrera) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+        this.carrera = carrera;
+
+        }
+
         public Alumno() {
 
         }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getApellido() {
+            return apellido;
+        }
+
+
 
 
 }
