53f0fd580eb1db11cb69d1fe4e6872d5eb20dbd9
POO <y.huang01@ufromail.cl>
2026-04-28 15:37:58 -0400

    pequeño cambio en el nombre de la clase, aplicando las buenas prácticas y atributos de Alumno

---
+++ src/Alumno.java
@@ -0,0 +1,11 @@
+public class Alumnos {
+    private String nombre;
+    private String apellido;
+    private String matricula;
+    private String carrera;
+        public Alumno() {
+
+        }
+
+
+}
