4694eafdae9f25e1a91ecd276265eefd35aebe0e
POO <y.huang01@ufromail.cl>
2026-04-28 22:03:11 -0400

    se comprueba si es que funciona el código

--- src/Launcher.java
+++ src/Launcher.java
@@ -13,5 +13,19 @@ public class Launcher {
         curso.agregarAlumno(alumno4);
         curso.agregarAlumno(alumno5);
 
+        System.out.println("Lista inicial de alumnos:");
+        System.out.println(curso.toString());
+
+        System.out.println("Eliminar alumno3 de la lista");
+        curso.eliminarAlumno(alumno3);
+        System.out.println("Lista de alumnos después de eliminar alumno3:");
+        System.out.println(curso.toString());
+
+        System.out.println("Comprobar si alumno4 pertenece al curso:");
+        curso.comprobarAlumno(alumno4);
+        System.out.println("Comprobar si alumno3 pertenece al curso:");
+        curso.comprobarAlumno(alumno3);
+
+
     }
 }
